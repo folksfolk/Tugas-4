8710443fa5a6500b7e1fbd214c17862dc72505fa
folksfolk <dhaniiihh@gmail.com>
2020-08-28 04:05:42 +0700

    tambah tugas

---
+++ tugas4.js
@@ -0,0 +1,52 @@
+// Jawaban Nomor 1
+var a = 2;
+var b = 20;
+var looping1 = "";
+var looping2 = "";
+
+console.log("Looping Pertama");
+while ( a <= 20) {
+    if (a % 2 == 0) {
+        looping1 = console.log(a + " - I Love Coding");
+    }
+    a ++;
+}
+
+console.log("Looping Kedua");
+while (b >= 2) {
+    if (b % 2 == 0) {
+        looping2 = console.log(b + " - I Will Become a FrontEnd Developer");
+    }
+    b--;
+}
+// Jawaban Nomor 2
+for (var c = 1; c <= 20; c++) {
+    if (c % 2 == 1) {
+        if (c % 3 == 0) {
+            console.log(c + " - I Love Coding");
+        } else {
+            console.log(c + " - Santai");
+        }
+    } else {
+        console.log(c + " - Berkualitas");
+    }
+}
+
+// Jawaban Nomor 3
+// saya gak tau mas, nyerah
+
+// Jawaban Nomor 4
+var kalimat = "saya sangat senang belajar javascript"
+var nama = kalimat.split(" ")
+console.log(nama)
+
+// Jawaban Nomor 5
+var daftarBuah = ["2. Apel", "5. Jeruk", "3. Anggur", "4. Semangka", "1. Mangga"];
+
+daftarBuah.sort();
+for (var i = 0; i < daftarBuah.length; i++) {
+    console.log(daftarBuah[i]);
+}
+
+
+
